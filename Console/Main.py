e97a38e54916457fccf82de3cdd341dd86c0bf47
Felipe-Souza-Pereira-Lima <70407660+Felipe-Souza-Pereira-Lima@users.noreply.github.com>
2020-08-29 16:31:16 -0300

    Update Main.py

--- Console/Main.py
+++ Console/Main.py
@@ -1,6 +1,31 @@
-import setuptools
-
-__version__ = '1.0.7'
-
-if __name__ == '__main__':
-   setuptools.setup("Console", version)
+# -*-coding-*- utf-8
+from time import sleep, strftime
+from os import startfile, system
+from random import randint, choice
+__version__ = '1.0.2'
+if __name__ == "__main__":
+   class a:
+      __module__ = f'Main.py -> Console version {__version__}'
+      __slots__ = {}
+      __annotations__ = {}
+      def __init__(self):
+         keywords = []
+         for key in keywords:
+            return True
+         else:
+            return False
+      def __reduce__(text, value, testize):
+         try:
+            keys = [testize, slice, value]
+            while True:
+               for i in keys:
+                  return i
+               else:
+                  i = False
+                  return i
+         except:
+            i = None
+            continue
+         return i
+input()
+         
