f01bd240932f0d655d4d5d70ea596e06ef8b70c0
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-09-05 22:33:15 -0300

    POO and new function

--- Scripta/__init__.py
+++ Scripta/__init__.py
@@ -7,61 +7,66 @@ __package__ = 'Scripta'
 __language__ = 'Português (Brasil)'
 
 # Inicia o código
-def init():
-    pass
+Class Scripta:
+    def init():
+        pass
 # Escreva na tela
-def echo(text):
-    print(text)
+    def echo(text):
+        print(text)
 # Adiciona mais cmmandos na linha
-def additions(code):
-    code
+    def additions(code):
+        code
 # Conecta com servidor IPV4\TCP
-def connect_to(ip, port):
-    try:
-        c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        c.connect(ip, port)
-    except:
-        print('Erro ao conectar!')
-    else:
-        print(f'Conectado com sucesso a IP: [{ip}] Porta: [{port}]')
+    def connect_to(ip, port):
+        try:
+            c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            c.connect(ip, port)
+        except:
+            print('Erro ao conectar!')
+        else:
+            print(f'Conectado com sucesso a IP: [{ip}] Porta: [{port}]')
 # Cria servidor socket IPV4\TCP
-def server_init(ip, port, listen):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((ip, port))
-        s.listen(listen)
-    except:
-        print('Erro ao criar o servidor!')
-    else:
-        print('Servidor criado com sucesso!')
-        s.accept()
+    def server_init(ip, port, listen):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.bind((ip, port))
+            s.listen(listen)
+        except:
+            print('Erro ao criar o servidor!')
+        else:
+            print('Servidor criado com sucesso!')
+            s.accept()
 # Executa comandos do sistema
-def command(systemcommand):
-    system(systemcommand)
-    pass
+    def command(systemcommand):
+        system(systemcommand)
+        pass
 # Esperar
-def wait(seconds):
-    try:
-        sleep(seconds)
-    except:
-        print('TypeError não é possivel esperar strings ou TrueValues\n')
+    def wait(seconds):
+        try:
+            sleep(seconds)
+        except:
+            print('TypeError não é possivel esperar strings ou TrueValues\n')
 # Executa programas .exe
-def exe(filelocal):
-    try:
-        startfile(filelocal)
-    except:
-        print('Esse programa não existe!')
-    else:
-        print(f'{filelocal} Foi iniciado!')
+    def exe(filelocal):
+        try:
+           startfile(filelocal)
+        except:
+            print('Esse programa não existe!')
+        else:
+            print(f'{filelocal} Foi iniciado!')
 # Condição rapida
-def maybe(comparador, repositorio, SeTrue, SeFalse):
-    if comparador == repositorio:
-        SeTrue
-    else:
-        SeFalse
+    def maybe(comparador, repositorio, SeTrue, SeFalse):
+        if comparador == repositorio:
+            SeTrue
+        else:
+            SeFalse
+            
+    def open_write(to_open, str, mode='r'):
+        with open(to_open, mode) as f:
+            write(f, str)
 # Tentar de uma linha
-def trying(tentar, mensagem_de_erro):
-    try:
-        tentar
-    except:
-        print(mensagem_de_erro)
+    def trying(tentar, mensagem_de_erro):
+        try:
+            tentar
+        except:
+             print(mensagem_de_erro)
